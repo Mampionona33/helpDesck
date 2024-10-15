151af97d64fd77eb787cad6137ce45f938265a5e
Mampionona33 <ramamps33@gmai.com>
2024-10-15 03:56:04 +0300

    mise en place page login

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -3,7 +3,10 @@ import React from "react";
 const Login = () => {
   return (
     <div className="h-screen flex flex-col w-screen justify-center items-center bg-gradient-to-t from-gradient-start to-gradient-end">
-      <h1 className="text-white">Login</h1>
+      <form className="flex flex-col gap-4 bg-slate-200 h-2/3 w-1/3 p-4 ">
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" id="email" />
+      </form>
     </div>
   );
 };
